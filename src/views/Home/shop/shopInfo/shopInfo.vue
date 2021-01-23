6716e2d45f8817179fcc838b866b42025d07ded8
Krega0129 <1841628911@qq.com>
2021-01-24 02:12:56 +0800

    获取店铺证书

--- src/views/Home/shop/shopInfo/shopInfo.vue
+++ src/views/Home/shop/shopInfo/shopInfo.vue
@@ -25,6 +25,7 @@
           <v-spacer></v-spacer>
           <v-btn
             dark
+            class="mr-5"
             @click="_shopIdGetCommodity"
           >
             店铺商品
@@ -73,6 +74,24 @@
               </v-data-table>
             </v-card>
           </v-dialog>
+          <v-btn
+            dark
+            @click="_getShopLicense"
+          >
+            店铺证书
+          </v-btn>
+          <v-dialog v-model="showLicense" max-width="700px">
+            <v-card class="d-flex justify-space-between flex-wrap pa-5">
+              <div class="text-center my-2" v-for="item in license" :key="item.url">
+                <div>{{item.name}}</div>
+                <v-img 
+                  max-width="200px" 
+                  max-height="200px" 
+                  :src="BASE_URL + '/' + item.url"
+                  @click="scaleImg(BASE_URL + '/' + item.url)"></v-img>
+              </div>
+            </v-card>
+          </v-dialog>
         </v-toolbar>
       </template>
     </v-data-table>
@@ -82,7 +101,8 @@
 <script>
   import {
     shopIdGetShop,
-    shopIdGetCommodity
+    shopIdGetCommodity,
+    shopIdGetShopLicense
   } from '../../../../network/shop';
   import { BASE_URL, H_config } from '../../../../network/config';
 
@@ -150,6 +170,7 @@
         ],
         shopCommodity: [],
         showSpecs: false,
+        showLicense: false,
         specsHeaders: [
           {
             text: '规格名称',
@@ -172,7 +193,8 @@
         ],
         specs: [],
         imgUrl: '',
-        showImg: false
+        showImg: false,
+        license: []
       }
     },
     async mounted() {
@@ -238,6 +260,30 @@
             this.specs.push(spec)
           }
         }
+      },
+      _getShopLicense() {
+        shopIdGetShopLicense({
+          shopId: this.$store.state.shopId
+        }).then(res => {
+          if(res.code === H_config.STATECODE_get_SUCCESS) {
+            let license = res.data
+            let keys = Object.keys(license)
+            for(let i in keys) {
+              let img = {}
+              switch(keys[i]) {
+                case 'busLicense': img.name = '营业执照'; break;
+                case 'cardBack': img.name = '手持身份证背面'; break;
+                case 'cardFront': img.name = '手持身份证正面'; break;
+                case 'restLicense': img.name = '餐饮许可证'; break;
+                case 'shopIn': img.name = '店铺内部照'; break;
+                case 'shopOut': img.name = '店铺外部照'; break;
+              }
+              img.url = license[keys[i]]
+              this.license.push(img)
+            }
+          }
+        })
+        this.showLicense = true
       },
       scaleImg(url) {
         this.imgUrl = url
